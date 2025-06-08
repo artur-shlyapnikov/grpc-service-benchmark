59484c5d90ce1315486e1c8d51f980e000566554
Artur Shlyapnikov <97292630+artur-shlyapnikov@users.noreply.github.com>
2025-06-08 20:46:32 +0300

    feat: make tests use configurable host

--- tests/jmeter-dsl/app/src/test/java/org/example/perf/grpc/ReliabilityTest.java
+++ tests/jmeter-dsl/app/src/test/java/org/example/perf/grpc/ReliabilityTest.java
@@ -46,8 +46,13 @@ class ReliabilityTest {
         static final int REQUIRED_STABLE_WINDOWS = 6;
     }
 
-    private static final String TEST_HOST = "162.55.34.236";
-    private static final String INFLUX_URL = "http://162.55.34.236:8086/write?db=perf-tests";
+    private static final String TEST_HOST = System.getProperty(
+            "test.host",
+            System.getenv().getOrDefault("TEST_HOST", "localhost"));
+    private static final String INFLUX_URL = System.getProperty(
+            "influx.url",
+            System.getenv().getOrDefault("INFLUX_URL",
+                    "http://localhost:8086/write?db=perf-tests"));
     private static final int TEST_PORT = 50052;
 
     // reusing records from MaximumLoadTest
